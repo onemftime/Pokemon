57eb1eec41f081118df7a399e4b1c1d368703c1b
Jeremy <jmshasha@gmail.com>
2017-07-22 12:59:22 -0700

    map installed

--- Pokemon/ViewController.swift
+++ Pokemon/ViewController.swift
@@ -7,15 +7,36 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    var manager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        manager.delegate = self
+       
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            
+            mapView.showsUserLocation = true
+            manager.startUpdatingLocation()
+            
+            
+        }else {
+            manager.requestWhenInUseAuthorization()
+        }
+        
     }
 
-   
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let region = MKCoordinateRegionMakeWithDistance(manager.location!.coordinate, 1000, 1000)
+        mapView.setRegion(region, animated: false)
+        
+    }
 
 }
 
